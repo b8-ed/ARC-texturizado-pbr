3f3d32bc3ea8811f18737dcacc0e567ecce21fcb
Luis Bazan <luisbazan417@gmail.com>
2017-11-26 22:22:19 -0600

    Substance metal oxidado y rotacion en Y agregado

---
+++ Assets/Test/LB/Scripts/SCR_ObjectView.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SCR_ObjectView : MonoBehaviour
+{
+
+    public float TurnX;
+    public float TurnY;
+    public float TurnZ;
+
+    public float MoveX;
+    public float MoveY;
+    public float MoveZ;
+
+    public bool World;
+    bool subiendo = true;
+    Vector3 startPos;
+    Vector3 endPos;
+
+    void Start()
+    {
+        startPos = transform.position;
+        endPos = new Vector3(transform.position.x + MoveX, transform.position.y + MoveY, transform.position.z + MoveZ);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (World == true)
+        {
+            transform.Rotate(TurnX * Time.deltaTime, TurnY * Time.deltaTime, TurnZ * Time.deltaTime, Space.World);
+            //transform.Translate(MoveX * Time.deltaTime, MoveY * Time.deltaTime, MoveZ * Time.deltaTime, Space.World);
+
+            if (subiendo == true)
+            {
+                transform.position = Vector3.Lerp(startPos, endPos, Time.time);
+                if (transform.position == endPos)
+                {
+                    subiendo = false;
+                }
+            }
+
+            else
+            {
+                transform.position = Vector3.Lerp(endPos, startPos, Time.time);
+                if (transform.position == startPos)
+                {
+                    subiendo = true;
+                }
+            }
+
+        }
+        else
+        {
+            transform.Rotate(TurnX * Time.deltaTime, TurnY * Time.deltaTime, TurnZ * Time.deltaTime, Space.Self);
+            transform.Translate(MoveX * Time.deltaTime, MoveY * Time.deltaTime, MoveZ * Time.deltaTime, Space.Self);
+        }
+    }
+}
