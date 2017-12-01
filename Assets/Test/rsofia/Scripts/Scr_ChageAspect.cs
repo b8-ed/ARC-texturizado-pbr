39fb9826865a057bf1b63d332364432406385065
Sofia Brown <ruthsofiab@gmail.com>
2017-12-01 09:41:06 -0600

    avances

---
+++ Assets/Test/rsofia/Scripts/Scr_ChageAspect.cs
@@ -0,0 +1,39 @@
+﻿//ruth sofia brown
+//git rsofia
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Scr_ChageAspect : MonoBehaviour
+{
+    public GameObject target;
+    public Texture normal;
+
+
+	public void DisplayWireframe()
+    {
+        
+    }
+
+    public void DisplayAlbedo()
+    {
+
+    }
+
+    public void DisplayNormal()
+    {
+        target.GetComponent<Renderer>().material.SetTexture(Shader.PropertyToID("_MainTex"), normal);
+    }
+
+    public void DisplayMetallic()
+    {
+
+    }
+
+    public void DisplayRoughness()
+    {
+
+    }
+}
