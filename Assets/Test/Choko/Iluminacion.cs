f6fac9b0b65ebe485e0fe0a1d5df9af79cc8152a
Luis Valera <luis_luis_260@hotmail.com>
2017-12-08 12:19:40 -0600

    Rig iluminacion actualizacion

--- Assets/Test/Choko/Iluminacion.cs
+++ Assets/Test/Choko/Iluminacion.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class Iluminacion : MonoBehaviour {
 
     public Light BackLight;
@@ -9,17 +9,18 @@ public class Iluminacion : MonoBehaviour {
     public Light FillLight;
 
     public float f_spotAngle;
-
+    public float f_range;
 
     public float RadioAlCentro;
 
     public float f_AlturaSet;
+    public float rotation;
 
     void Start ()
     {
         Triangular();
-            CambiarLucesADireccional();
-
+        CambiarLucesADireccional();
+        
     }
 
     void Update ()
@@ -28,33 +29,78 @@ public class Iluminacion : MonoBehaviour {
             CambiarLucesADireccional();
         if (Input.GetKeyDown(KeyCode.S))
             CambiarLucesAPoint();
+        if (Input.GetKeyDown(KeyCode.Q))
+            ApagarKeyLight();
+        if (Input.GetKeyDown(KeyCode.W))
+            ApagarBackLight();
+        if (Input.GetKeyDown(KeyCode.E))
+            ApagarFillLightt();
 
         SeguirObjeto();
     }
 
-
-    void Triangular()
+  
+    //Apaga la luz key
+    public void ApagarKeyLight()
     {
-        KeyLight.gameObject.transform.position = new Vector3(RadioAlCentro, f_AlturaSet, -RadioAlCentro / 2);
-        BackLight.gameObject.transform.position = new Vector3(0, f_AlturaSet, RadioAlCentro);
-        FillLight.gameObject.transform.position = new Vector3(-RadioAlCentro, f_AlturaSet, -RadioAlCentro / 2);
+        if (KeyLight.gameObject.activeSelf)
+            KeyLight.gameObject.SetActive(false);
+        else
+            KeyLight.gameObject.SetActive(true);
 
     }
+    //Apaga la luz Back
+    public void ApagarBackLight()
+    {
+        if (BackLight.gameObject.activeSelf)
+            BackLight.gameObject.SetActive(false);
+        else
+            BackLight.gameObject.SetActive(true);
+    }
+    //Apaga la luz Fill
+    public void ApagarFillLightt()
+    {
+        if (FillLight.gameObject.activeSelf)
+            FillLight.gameObject.SetActive(false);
+        else
+            FillLight.gameObject.SetActive(true);
+    }
+    //actualiza y pone las luces en orden
+    public void Triangular()
+    {
 
+        gameObject.transform.Rotate(new Vector3(0, 1, 0), rotation * Time.deltaTime);
+
+        GameObject tmp = gameObject;
+        GameObject t0 = KeyLight.gameObject;
+        GameObject t1 = BackLight.gameObject;
+        GameObject t2 = FillLight.gameObject;
+
+
+        KeyLight.gameObject.transform.localPosition = new Vector3(RadioAlCentro, f_AlturaSet, -RadioAlCentro / 2);
+        BackLight.gameObject.transform.localPosition = new Vector3(0, f_AlturaSet, RadioAlCentro);
+        FillLight.gameObject.transform.localPosition = new Vector3(-RadioAlCentro, f_AlturaSet, -RadioAlCentro / 2);
+
+        KeyLight.transform.rotation = t0.transform.rotation;
+        BackLight.transform.rotation = t1.transform.rotation;
+        FillLight.transform.rotation = t2.transform.rotation;
+        gameObject.transform.rotation = tmp.transform.rotation;
+    }
+    //cambiar las luces a modo direccional
     public void CambiarLucesADireccional()
     {
         BackLight.type = LightType.Spot;
         KeyLight.type = LightType.Spot;
         FillLight.type = LightType.Spot;
     }
-
+    //Cambiar las luces a modo de punto
     public void CambiarLucesAPoint()
     {
         BackLight.type = LightType.Point;
         KeyLight.type = LightType.Point;
         FillLight.type = LightType.Point;
     }
-
+    //Ve al centro / puede cambiar a objeto
     void SeguirObjeto()
     {
         BackLight.transform.LookAt(Vector3.zero);
@@ -64,6 +110,10 @@ public class Iluminacion : MonoBehaviour {
         BackLight.spotAngle = f_spotAngle;
         KeyLight.spotAngle = f_spotAngle;
         FillLight.spotAngle = f_spotAngle;
+
+        BackLight.range = f_range;
+        KeyLight.range = f_range;
+        FillLight.range = f_range;
 
         Triangular();
     }
